10a2883dee4f952e81977b88e17ee51558dfc7d7
JEHYUNLIM <ljehyun@gmail.com>
2019-11-29 11:26:20 +0900

    Remove function update

--- src/components/App.js
+++ src/components/App.js
@@ -57,12 +57,25 @@ class App extends Component {
         });
     }
 
+    handleRemove = (id) => {
+        const {todos} = this.state;
+        const index = todos.findIndex(todo => todo.id === id);
+
+        this.setState({
+            todos:[
+                ...todos.slice(0,index),
+                ...todos.slice(index+1, todos.length)
+            ]
+        });
+    }
+
     render(){
         const {input, todos} = this.state;
         const {
             handleChange,
             handleInsert,
-            handleToggle
+            handleToggle,
+            handleRemove
         } = this;
 
 
@@ -70,7 +83,7 @@ class App extends Component {
             <div>
                 <PageTemplate>
                     <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
-                    <TodoList todos={todos} onToggle={handleToggle}/>
+                    <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
                 </PageTemplate>
             </div>
         );
