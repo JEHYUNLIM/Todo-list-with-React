5dbb3d58a7be129865c90981c124c3aa823be598
JEHYUNLIM <ljehyun@gmail.com>
2019-12-02 14:27:50 +0900

    improve performance by shouldComponentUpdate method

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import PageTemplate from './PageTemplate';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
+ const initialTodos = new Array(500).fill(0).map(
+     (foo, index)=>({id: index, text: `Todo ${index}`, done: false})
+ );
+
 class App extends Component {
     state = {
         input: '',
-        todos: [
-            {id:0, text: 'Study', done: true},
-            {id:1, text: 'Exercise', done: false}
-        ]
+        todos: initialTodos
     }
 
     id = 1;
